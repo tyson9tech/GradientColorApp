42e34e2626938406c615a161d3dcc3ff41755f68
tyson9tech <tyson9tech@gmail.com>
2021-02-08 15:08:15 -0500

    Take new code

---
+++ app.js
@@ -0,0 +1,10 @@
+const idBtn = document.getElementById('changerCouleur');
+const text = document.getElementById('essais');
+
+idBtn.addEventListener('click', ()=>{
+    text.style.color = newCouleur();
+});
+
+function newCouleur(){
+    
+}
