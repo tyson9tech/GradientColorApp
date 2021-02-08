ea9d915fa6269b16e1125848f0edefe8985b4734
tyson9tech <tyson9tech@gmail.com>
2021-02-08 15:38:39 -0500

    Ajout de nouvelles fonctionalite

--- app.js
+++ app.js
@@ -6,5 +6,19 @@ idBtn.addEventListener('click', ()=>{
 });
 
 function newCouleur(){
-    
+
+    let couleur = '#';
+
+    let caracters = [0,1,2,3,4,5,6,7,8,9,'a','b','c','d','e','f'];
+
+    while(couleur.length <= 6){
+        couleur += caracters[numRand(caracters[0], caracters.length)];
+    }
+
+    return couleur;
+}
+
+function numRand(min, max){
+    let num = (Math.random() * (max-min)) + min;
+    return parseInt(num);
 }
