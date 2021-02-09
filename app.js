82efc2b950ba80126789d3ad539d704f06e9835b
tyson9tech <tyson9tech@gmail.com>
2021-02-08 22:18:46 -0500

    Ajout de nouvelles fonctionalites a nouveau

--- app.js
+++ app.js
@@ -1,17 +1,38 @@
+//SELECTIONS
+const copyText = document.getElementById("texte");
 const idBtn = document.getElementById('changerCouleur');
-const text = document.getElementById('texte');
+const copie = document.getElementById('copie');
 const body = document.querySelector('body');
 
+
+//EVENTS
 idBtn.addEventListener('click', ()=>{
+
     let sides = ['to left', 'to right', 'to bottom', 'to top'];
 
     let newSide = sides[numRand(0, sides.length)];
 
     let newColor = `linear-gradient(${newSide}, ${newCouleur()}, ${newCouleur()})`;
     body.style.background = newColor;
-    text.innerHTML = `background : ${newColor};`;
+    copyText.value = `background : ${newColor};`;
+    copyText.disabled = false;
 });
 
+copie.addEventListener('click', ()=>{
+    if(copyText.value !== ""){
+        /* Select the text field */
+        copyText.select();
+        copyText.setSelectionRange(0, 99999); /* For mobile devices */
+
+        /* Copy the text inside the text field */
+        document.execCommand("copy");
+
+        /* Alert the copied text */
+        console.log("Copied the text: " + copyText.value);
+    }
+});
+
+//FUNCTIONS
 function newCouleur(){
 
     let couleur = '#';
